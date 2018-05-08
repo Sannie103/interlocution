7a47641f1ebf83e5fdaad5cf6abd4556c52be3e8
company <company@company.local>
2018-05-08 15:37:12 +0800

    Initial Commit

---
+++ Interlocution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Interlocution
+//
+//  Created by company on 2018/5/8.
+//  Copyright © 2018年 company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InterlocutionTests/InterlocutionTests.m
@@ -0,0 +1,39 @@
+//
+//  InterlocutionTests.m
+//  InterlocutionTests
+//
+//  Created by company on 2018/5/8.
+//  Copyright © 2018年 company. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InterlocutionTests : XCTestCase
+
+@end
+
+@implementation InterlocutionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
